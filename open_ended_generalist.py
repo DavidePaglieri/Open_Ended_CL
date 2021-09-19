5ee69ed08838169a22b02495efeab0b631c59648
Davide <paglieridavide@gmail.com>
2021-09-19 15:34:07 +0200

    Updating information

--- open_ended_generalist.py
+++ open_ended_generalist.py
@@ -1,5 +1,18 @@
-# Re-implmenetation of POET with ARS instead of ES and with A1 robot on
-# procedurally generated environments
+# Copyright 2021 Davide Paglieri
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Open-Ended Curriculum Learning algorithm by Davide Paglieri at Imperial College London
 
 import numpy as np
 import multiprocessing as mp
